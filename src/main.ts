d2a8684af968363170511bd9b28008619865064b
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-13 16:02:29 -0300

    Estrutura Inicial

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import './style.css';
+import Slide from './Slide';
+
+const container = document.getElementById('slide');
+const elements = document.getElementById('slide-elements');
+const controls = document.getElementById('slide-controls');
+
+if (container && elements && controls && elements.children.length) {
+  const slide = new Slide(
+    container,
+    Array.from(elements?.children),
+    controls,
+    3000
+  );
+}
